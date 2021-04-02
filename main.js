db0cf47d9734191a798adf05e0c9c8630a0722db
shj3497 <shj3497@gmail.com>
2021-04-02 22:13:40 +0900

    JavaScript navbar menu item

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 
 document.addEventListener('scroll', () => {
-    console.log(`a >>> : ` + window.scrollY);
-    console.log(`navbarHeight >>> : ${navbarHeight}`);
+    //console.log(`a >>> : ` + window.scrollY);
+    //console.log(`navbarHeight >>> : ${navbarHeight}`);
 
     if(window.scrollY > navbarHeight){
         navbar.classList.add(`navbar--dark`);
@@ -14,3 +14,24 @@ document.addEventListener('scroll', () => {
         navbar.classList.remove(`navbar--dark`);
     }
 });
+
+// Handle Scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+    // data-link="#home"
+    // event.target : click이벤트가 발생한 target반환
+    // .dataset >> data- 로 시작하는 속성을 의미
+    // .link >> data-link이니까 link
+    
+    const target = event.target;
+    const link = target.dataset.link;
+    if(link == null){
+        return;
+    }
+    console.log(`link >>> : ${link}`);
+    const scrollTo = document.querySelector(link);
+    
+    scrollTo.scrollIntoView({
+        behavior: 'smooth'
+    });
+})
