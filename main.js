76f48e1641898393dd09ee09feeb0247265e91f1
shj3497 <shj3497@gmail.com>
2021-04-03 23:45:40 +0900

    CSS Final touch1

--- main.js
+++ main.js
@@ -147,8 +147,6 @@ workBtnContainer.addEventListener('click', (event) => {
 
 // Navbar toggle button for small screen
 const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');;
-
-console.log(navbarToggleBtn);
 navbarToggleBtn.addEventListener('click', () => {
     navbarMenu.classList.toggle('open');
 })
